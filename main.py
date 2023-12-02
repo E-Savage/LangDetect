2461386c0bf6ba1a4e816df63209f76ff7b02e12
E-Savage <eric.n.savage2000@gmail.com>
2023-12-01 21:48:50 -0500

    made changes to the main.py file to make it smaller

--- main.py
+++ main.py
@@ -10,36 +10,8 @@ from sklearn.naive_bayes import MultinomialNB
 
 global title
 
-# curses.title("Language Detective")
-
-
 def main(scr):
-    curses.echo()
-
-    start_here = 11
-
     running = True
-
-    #mw = curses.newwin(0, 0)
-
-    # scr.addstr(title)
-    # scr.refresh()
-    # scr.getch()
-
-    # row, cols = scr.getmaxyx()
-
-    
-    # main_win.box()
-    # scr.keypad(True)
-    # scr.refresh()
-
-    # scr.addstr(start_here, 0, "This is a test line")
-
-    # scr.getch()
-    #curses.napms(2000)
-    #print_title(scr)
-    #scr.refresh()
-
 
     title = """
     (                                             (                                       
@@ -53,62 +25,61 @@ def main(scr):
                     |___/           |___/                                                  
     """
 
-    scr.addstr(0, 0, title)
-    # scr.addstr(11, 4, "teehee") start menu items here  
-    # scr.addstr(1, 4, "@") this is where you need to start at for everything else    
-    scr.refresh()
     items = ["Language Detection", "Help Page", "About page", "Exit"]
 
     while running:
+        scr.addstr(0,0, title)
+        scr.refresh()
         menu = Menu(items, scr, 11, 4)
         result = menu.run()
+        win = curses.newwin(9, 86, 16, 4)
 
         if items[result] == 'Language Detection':
-            scr.addstr(1, 1, f"{items[result]}")
-            scr.refresh()
-            '''
-            if this is selected it should go into the main app which is language detection
-            '''
+            #here is something
+            t = 4
+            
         elif items[result] == 'Help Page':
-            scr.addstr(1, 1, f'{items[result]}')
-            scr.refresh()
             '''
             if this is selected should display a sub window with the stuff in it 
             '''
-        elif items[result] == 'About page':
-            scr.addstr(1, 1, f'{items[result]}')
+            #                   h   w   y   x
+            win.clear()
+            win.touchwin()
             scr.refresh()
+            win.box()
+            win.addstr(0, 0, "Help Page") 
+            win.addstr(1, 1, "(1) Language detection, option takes user to language detection page  allows user to")
+            win.addstr(2, 5, "enter text to determine language of the text, to quit this mode press q and then")
+            win.addstr(3, 5, "enter to be taken back to this main screen")
+            win.addstr(4, 1, "(2) Help Page, it pulled up this tiny window you are looking at now. It gives you")
+            win.addstr(5, 5, "information on how to use the program and what each button does.")
+            win.addstr(6, 1, "(3) About Page, this page tells you more about the program and it purpose")
+            win.addstr(7, 1, "(4) Exit, quits the program")
+            win.refresh()
+            scr.refresh()
+        elif items[result] == 'About page':
             '''
             if this is selected it should display a subwindow with stuff in it, the stuff is about the about page
             '''
+            win.clear()
+            win.touchwin()
+            scr.refresh()
+            win.box()
+            win.addstr(0, 0, "About Page")
+            win.addstr(1, 1, "This app is a final project for CIS 542 Digital Forensics. Topic chosen was")
+            win.addstr(2, 1, "detection of languages from text or audio. I decided to go with text. This")
+            win.addstr(3, 1, "project's purpose was to give me a tutorial on how to incorporate ML in to")
+            win.addstr(4, 1, "a terminal based application using Python and the curses library. Also,")
+            win.addstr(5, 1, "this project allowed me to learn python curses as well. Overall, this")
+            win.addstr(6, 1, "project was fun to program and learn from. In the future I would like to")
+            win.addstr(7, 1, "add more features and flesh the app out more. Thank you for using my app!")
+            win.refresh()
+            scr.refresh()
         elif items[result] == 'Exit':
             running = False
         else:
             pass
         scr.refresh()
-
-# def print_title(scr):
-#     title = """
-#      (                                             (                                       
-#      )\ )                                          )\ )          )           )             
-#     (()/(    )       (  (    (     ) (  (    (    (()/(    (  ( /(  (     ( /((   )     (  
-#      /(_))( /(  (    )\))(  ))\ ( /( )\))(  ))\    /(_))  ))\ )\())))\ (  )\())\ /((   ))\ 
-#     _))  )(_)) )\ )((_))\ /((_))(_)|(_))\ /((_)  (_))_  /((_|_))//((_))\(_))((_|_))\ /((_)
-#     | |  ((_)_ _(_/( (()(_|_))(((_)_ (()(_|_))     |   \(_)) | |_(_)) ((_) |_ (_))((_|_))  
-#     | |__/ _` | ' \)) _` || || / _` / _` |/ -_)    | |) / -_)|  _/ -_) _||  _|| \ V // -_) 
-#     |____\__,_|_||_|\__, | \_,_\__,_\__, |\___|    |___/\___| \__\___\__| \__||_|\_/ \___| 
-#                     |___/           |___/                                                  
-#     """
-
-#     rows = title.strip().splitlines()
-
-#     title_list = [list(row) for row in rows]
-
-#     for i, row in enumerate(title_list):
-#         for j, char in enumerate(row):
-#             scr.addstr(i, j, char)
-
-#     scr.refresh()
 
 def menu_print(items, scr):
     curses.curs_set(0)
@@ -118,3 +89,4 @@ def menu_print(items, scr):
 
 if __name__ == "__main__":
     curses.wrapper(main)
+
